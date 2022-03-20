e550f2d1c50b5a73f3b3c07043776a2c08b2df85
mai shani <mai96shani@gmail.com>
2022-03-20 22:52:03 +0200

    change client file

--- engine/src/client.java
+++ engine/src/client.java
@@ -1,13 +1,20 @@
+import java.util.Map;
+import java.util.Set;
+
 public class client {
     private String fullName;
-    public String getFullName(){
-        return fullName;
-    };
+    private int currBalance;
+    private Set<loan> asGiver;
+    private Set<loan> asBorrower;
+    private Map<Integer,Set<movement>> movements;
+
+    public client(String name, int accountBalance) {
+        fullName=name;
+        currBalance=accountBalance;
+    }
+
     protected void setName(String name){
         this.fullName = name;
     };
 
-    client(String fullName){
-        this.fullName = fullName;
-    }
 }
