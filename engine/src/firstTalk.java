09fe678906ffaf33253b075817571a2d1d4ef448
inbal <inbal.nizani@gmail.com>
2022-03-20 15:14:07 +0200

    first commit

---
+++ engine/src/firstTalk.java
@@ -0,0 +1,5 @@
+public class firstTalk {
+    public int func(int a) {
+        return a + 1;
+    }
+}
---
+++ ui/src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello please insert a number:");
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a);
+        firstTalk hi= new firstTalk();
+       a= hi.func(a);
+System.out.println(a);
+    }
+}
