b2327145b66c7b0c585033e12a96f66748669c62
inbal <inbal.nizani@gmail.com>
2022-04-05 20:35:04 +0300

    p

--- engine/src/abs/PayBack.java
+++ engine/src/abs/PayBack.java
@@ -3,27 +3,24 @@ package abs;
 public class PayBack {
 
     private Client givesALoan;
-<<<<<<< HEAD
     private double percentage;
     private double originalAmount;
-public PayBack(Client client, double originalAmount,int investorAmount) {
-    this.givesALoan = client;
-    this.percentage = investorAmount/originalAmount;
-    this.originalAmount = originalAmount;
-}
-=======
-    private int originalAmount;
-
+    public PayBack(Client client, double originalAmount,int investorAmount) {
+        this.givesALoan = client;
+        this.percentage = investorAmount / originalAmount;
+        this.originalAmount = originalAmount;
+    }
+    
     public PayBack(Client client, int originalAmount) {
         this.givesALoan = client;
         this.originalAmount = originalAmount;
     }
 
-    public int getOriginalAmount() {
+    public double getOriginalAmount() {
         return originalAmount;
     }
 
->>>>>>> main
+
     public Client getClientDTOGivers() {
         return givesALoan;
     }
@@ -31,17 +28,9 @@ public PayBack(Client client, double originalAmount,int investorAmount) {
     public void setGivesALoan(Client client) {
         givesALoan = client;
     }
-<<<<<<< HEAD
-    public double getPercentage(){
-    return percentage;
-=======
 
-    public void setOriginalAmount(int amount) {
-        originalAmount = amount;
->>>>>>> main
-    }
-    public double getOriginalAmount(){
-    return originalAmount;
+    public double getPercentage() {
+        return percentage;
     }
 
 
