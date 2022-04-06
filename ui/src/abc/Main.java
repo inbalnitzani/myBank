6a74f1405ec9c639637de0abbccece1a70d4927e
mai shani <mai96shani@gmail.com>
2022-04-06 11:57:31 +0300

    fix double

--- ui/src/abc/Main.java
+++ ui/src/abc/Main.java
@@ -4,6 +4,8 @@ import abs.Globals;
 
 public class Main {
     public static void main(String[] args) {
+        double x=16/5;
+double y=16%5;
         TaskManager manager = new TaskManager();
         manager.manageSystem();
     }
