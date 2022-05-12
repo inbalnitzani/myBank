4b53a1ef1985a5342df470ddc6a86bbcb7351a56
inbal <inbal.nizani@gmail.com>
2022-05-12 19:09:13 +0300

    sss

--- javafx/src/app/bodyUser/bodyUser.java
+++ javafx/src/app/bodyUser/bodyUser.java
@@ -3,10 +3,14 @@ package app.bodyUser;
 import app.main.AppController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class bodyUser {
 
     @FXML private Label body;
+    @FXML private Tab information,scramble,payment;
+
     private AppController mainController;
 
     public void setMainController(AppController mainController) {
