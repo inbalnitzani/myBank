ab68858b00690250538712d7740f4c7bfa7a13d8
mai shani <mai96shani@gmail.com>
2022-05-15 13:22:04 +0300

    update user body

--- javafx/src/app/bodyUser/bodyUser.java
+++ javafx/src/app/bodyUser/bodyUser.java
@@ -1,18 +1,32 @@
 package app.bodyUser;
-
+import app.information.informationController;
+import app.inlayController.inlayController;
 import app.main.AppController;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.Parent;
+
+import java.util.List;
 
 public class bodyUser {
 
-    @FXML private Label body;
-    @FXML private Tab information,scramble,payment;
+    @FXML private inlayController scrambleComponentController;
+    @FXML private Parent scrambleComponent;
+    @FXML private informationController informationComponentController;
+    @FXML private Parent informationComponent;
+    public AppController mainController;
 
-    private AppController mainController;
+    @FXML public void initialize(){
+        if (scrambleComponentController!=null && informationComponentController!=null) {
+            this.scrambleComponentController.setBodyUser(this);
+            this.informationComponentController.setBodyUser(this);
+        }
+    }
 
+    public bodyUser(){}
+    public void setData(){
+        scrambleComponentController.setCategories();
+    }
+    public List<String> getCategories(){return mainController.getCategories();}
     public void setMainController(AppController mainController) {
         this.mainController = mainController;
     }
---
+++ javafx/src/app/information/informationController.java
@@ -0,0 +1,11 @@
+package app.information;
+
+import app.bodyUser.bodyUser;
+
+public class informationController {
+
+    private bodyUser bodyUser;
+    public void setBodyUser(bodyUser bodyUser) {
+        this.bodyUser = bodyUser;
+    }
+}
