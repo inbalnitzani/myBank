977639fdab1fd69f81bb8d0fd70b12466defb5f8
inbal <inbal.nizani@gmail.com>
2022-05-16 17:19:24 +0300

    info

--- javafx/src/app/bodyUser/bodyUser.java
+++ javafx/src/app/bodyUser/bodyUser.java
@@ -1,20 +1,43 @@
 package app.bodyUser;
 
 import app.main.AppController;
+import dto.ClientDTO;
+import dto.LoanDTO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class bodyUser {
 
     @FXML private Label body;
     @FXML private Tab information,scramble,payment;
-
+    @FXML private information informationComponentController;
+    @FXML private Parent informationComponent;
     private AppController mainController;
 
+    private ClientDTO user;
+
+
+    public void setUser(ClientDTO user) {
+        this.user = user;
+        informationComponentController.setUser(user);
+    }
     public void setMainController(AppController mainController) {
         this.mainController = mainController;
     }
+    public void showData(ClientDTO clientDTO) {
+    informationComponentController.showData(clientDTO);
+    }
 
 }
+
+
+
--- javafx/src/app/main/AppController.java
+++ javafx/src/app/main/AppController.java
@@ -88,6 +88,14 @@ public class AppController {
                     bodyAdminController.showData();
                 } else {
                     mainComponent.setCenter(userComponentRoot);
+                    List<ClientDTO> clientDTOList = myBank.getClients();
+                    ClientDTO client = null;
+                    for (ClientDTO currClient:clientDTOList) {
+                       if (currClient.getFullName() == user)
+                           client = currClient;
+                    }
+                    bodyUserController.setUser(client);
+                    bodyUserController.showData(client);
                 }
             }
         }
